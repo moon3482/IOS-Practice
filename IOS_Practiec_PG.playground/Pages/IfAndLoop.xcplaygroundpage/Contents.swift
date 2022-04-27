95aea9d82b5a420a26b2ef9f5f357729f83b0a50
Charlie_Moon <moon3482@cau.ac.kr>
2022-04-27 16:37:53 +0900

    조건문

--- IOS_Practiec_PG.playground/Pages/IfAndLoop.xcplaygroundpage/Contents.swift
+++ IOS_Practiec_PG.playground/Pages/IfAndLoop.xcplaygroundpage/Contents.swift
@@ -1,7 +1,42 @@
-//: [Previous](@previous)
 
-import Foundation
 
-var greeting = "Hello, playground"
+//if문
+func dinner(){
+    var hasMoney = 2500
+    var menu = ""
+    if hasMoney <= 1000{
+        menu = "도시락 라면"
+    } else if hasMoney > 1000 && hasMoney <= 1500{
+        menu = "신라면"
+    } else if hasMoney > 1500 && hasMoney <= 2500 {
+        menu = "나가사키짬뽕라면 + 쿨피스"
+    }
+    else if hasMoney > 2500 && hasMoney <= 5000{
+        menu = "튀김우동라면 + 맥주 1캔"
+    }
+    else {
+        menu = "순대국"
+    }
+    print(menu)
+}
+dinner()
 
-//: [Next](@next)
+//swich 문
+func launch(){
+    var hasMoney = 2500
+    var menu = ""
+    switch hasMoney {
+    case 851..<1001:
+        menu = "도시락 라면"
+    case 1001..<1501:
+        menu = "신라면"
+    case 1499..<2501:
+        menu = "나가사키짬뽕라면 + 쿨피스"
+    case 2501..<5001:
+        menu = "튀김우동라면 + 맥주 1캔"
+    default:
+        menu = "순대국"
+    }
+    print(menu)
+}
+launch()
