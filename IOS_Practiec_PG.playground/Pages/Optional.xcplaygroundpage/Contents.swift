8fcc41744538558146337c8a8c170857e70dbd85
Charlie_Moon <moon3482@cau.ac.kr>
2022-05-11 17:27:08 +0900

    옵셔널 설명 주석 추가

--- IOS_Practiec_PG.playground/Pages/Optional.xcplaygroundpage/Contents.swift
+++ IOS_Practiec_PG.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -1,4 +1,9 @@
-
+/*
+ optional 의 필요성
+ nil의 가능성을 명시적으로 표현
+ 문서화 하지 않고 코드만 보고 nil의 가능성을 알 수 있음(주석/문서 작성시간 절약)
+ 예외 상황에 안전한 코딩
+ */
 //컴파일 에러
 //var name :String = "Charlie"
 //name = nil
@@ -18,3 +23,5 @@ var exportEmail:String? = email
 if let OpEmail = email{
     print(OpEmail)
 }
+
+
